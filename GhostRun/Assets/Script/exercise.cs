c182eeeeb3798ca1c840384c56fc1ce25e37bfe1
a123f456c789 <66512956+a123f456c789@users.noreply.github.com>
2020-07-04 21:00:39 +0800

    20200704

---
+++ GhostRun/Assets/Script/exercise.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class exercise : MonoBehaviour
+{
+    private void Start()
+    {
+       print(Mathf.PI);
+       print(Random.value);
+        print(Mathf.Abs(-10));
+        print(Random.Range(100.0f, 500.0f));
+    }
+    private void Update()
+    {
+       // print(Time.time);
+    }
+}
---
+++ GhostRun/Assets/Script/player.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    
+    /*
+    */
+    #region 角色
+    public float speed;
+    public float hp;
+    public float hight;
+    public int coin;
+    public bool dead;
+    public AudioClip CMusic;
+    public AudioClip JMusic;
+    public AudioClip SMusic;
+    #endregion
+
+    # region 方法
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Jump()
+    {
+        print("跳躍");
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Hit()
+    {
+        print("碰撞");
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Dead()
+    {
+
+    }
+    /// <summary>
+
+    /// 
+    /// </summary>
+    private void Coin()
+    {
+
+    }
+    /// <summary>
+    /// 
+    /// </summary>
+    private void Slide()
+    {
+
+    }
+    #endregion
+    #region 事件
+    private void Start()
+    {
+        Hit();   
+    }
+    private void Update()
+    {
+        Jump();
+    }
+    #endregion
+
+    //
+}
